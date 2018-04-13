3c3da5ca6c77bc2e6682bcd2e368bec8bf2b9686
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-04-13 19:46:35 -0300

    fix: added missing key to google maps api;

--- src/components/ContactMap/ContactMap.js
+++ src/components/ContactMap/ContactMap.js
@@ -5,7 +5,7 @@ import { compose, withProps } from 'recompose';
 
 export const ContactMap = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBUDsKbbMvYm2uGIqKmYn6lxkpPycr5KcY&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }}/>,
     containerElement: <div style={{ height: `400px` }}/>,
     mapElement: <div style={{ height: `100%` }}/>
